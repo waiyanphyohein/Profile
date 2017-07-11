76bc1c891c8165011200b925bec04dc8b9b85069
waiyanphyohein <waiyanphyohein@gmail.com>
2017-07-11 15:21:28 -0400

    "nothing has been changed really"

--- js/function.js
+++ js/function.js
@@ -7,7 +7,7 @@ function initMap()
     map = new google.maps.Map(document.getElementById('map'),{
         center: {lat: 40.7413549, lng: -73.9980244},
         zoom: 13
-    });
+    });   
     var Somet = map.getCenter();
    
 }  
